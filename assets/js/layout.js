6134160660a11ef0d9f0979a65e7ec16256cdfbe
Jenny <jenny@dynamsoft.com>
2020-11-18 16:07:15 +0800

    fix bug - scrollbar error

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,87 +1,68 @@
 $(document).ready(function(){
-    $('#sideBarCnt').css({'width': $('.sideBar').width() + 'px'})
-    $('.container .head').css({'width': $('.docContainer').width() + 'px'})
-    $('#fullTreeMenuListContainer').css({'height': 'calc(100vh - 245px)'})
-
-    if ($('.docContainer').height() + 125 > document.body.clientHeight) {
-        $('.history').addClass('history-fixed')
-    }
+    init()
 
     $(window).resize(function() {
-        $('#sideBarCnt').css({'width': $('.sideBar').width() + 'px'})
-        $('.container .head').css({'width': $('.docContainer').width() + 'px'})
+        init()
         realFunc()
     })
 
     window.addEventListener('scroll', realFunc);
 
-    function realFunc(){
-        var sd = $(window).scrollTop();
-        var dcheight = $('.docContainer').height() + 65 - sd
-        if (sd > 0 && dcheight < document.body.clientHeight) {
-            $('.history').removeClass('history-fixed')
-        } else {
-            if (!$('.history').hasClass('history-fixed')) {
-                $('.history').addClass('history-fixed')
-            }
-            if ($('.docContainer').height() + 125 < document.body.clientHeight) {
-                $('.history').removeClass('history-fixed')
-            }
+    function init() {
+        $('#sideBarCnt').css({'width': $('.sideBar').width() + 'px'})
+        $('.container .head').css({'width': $('.docContainer').width() + 'px'})
+        $('#fullTreeMenuListContainer').css({'height': 'calc(100vh - 245px)'})
+        $('.rightSideMenu').css({'height': 'calc(100vh - 255px)'})
+
+        if ($('.docContainer').height() + 125 >= document.body.clientHeight) {
+            $('.history').addClass('history-fixed')
+            $('#footerWrapper').css({'margin-top': '48px'})
         }
+    }
+
+    function realFunc() {
         if (breakpoint() == 'lg') {
-            if (sd > 65 && dcheight>0) {
-                if (!$('.subHeadWrapper').hasClass('shw-fixed')) {
-                    $('.subHeadWrapper').addClass('shw-fixed')
-                    $('.docContainer').addClass('dc-fixed')
-                    $('#sideBarCnt').addClass('ftm-fixed')
-                    $('.rightSideMenu').addClass('rsm-fixed')
-                    $('#docHead').addClass('ch-fixed')
-                }
-                if (dcheight > document.body.clientHeight) {
-                    $('#sideBarCnt.ftm-fixed').css({'height': 'calc(100vh - 120px)'})
-                    $('#sideBarCnt.ftm-fixed #fullTreeMenuListContainer').css({'height': 'calc(100vh - 180px)'})
-                    $('.rightSideMenu.rsm-fixed').css({'height': 'calc(100vh - 110px)'})
+            var sd = $(window).scrollTop();
+            var dcHeight = $('.docContainer').height() + 125 - sd
+            var clientHeight = document.body.clientHeight
+            if (sd >= 65) {
+                // head and sidebar fixed
+                $('.subHeadWrapper').css({'top': '0px'})
+                $('#docHead').css({'top': '60px'})
+                $('.sideBar').css({'padding-top': '0px'})
+                $('.sideBar #sideBarCnt').addClass('sidebar-fixed')
+                $('.rightSideMenu').addClass('rsm-fixed')
+                if (dcHeight + 48 > clientHeight) {
+                    // history fixed
+                    $('.history').addClass('history-fixed')
+                    $('#footerWrapper').css({'margin-top': '48px'})
                 } else {
-                    $('#sideBarCnt.ftm-fixed').css({'height': (dcheight - 40) + 'px'})
-                    $('#sideBarCnt.ftm-fixed #fullTreeMenuListContainer').css({'height': (dcheight - 90) + 'px'})
-                    $('.rightSideMenu.rsm-fixed').css({'height': (dcheight - 40) + 'px'})
+                    $('.history').removeClass('history-fixed')
+                    $('#footerWrapper').css({'margin-top': '0px'})
                 }
-                $('.markdown-body').css({'margin-top': (100 + $('.container .head').height()) + 'px'})
             } else {
-                $('.subHeadWrapper').removeClass('shw-fixed')
-                $('.docContainer').removeClass('dc-fixed')
-                $('#sideBarCnt').removeClass('ftm-fixed')
+                // head and sidebar fixed
+                $('.subHeadWrapper').css({'top': (65-sd) + 'px'})
+                $('#docHead').css({'top': (125-sd) + 'px'})
+                $('.sideBar').css({'padding-top': '60px'})
+                $('.sideBar #sideBarCnt').removeClass('sidebar-fixed')
                 $('.rightSideMenu').removeClass('rsm-fixed')
-                $('.container .head').removeClass('ch-fixed')
-                $('.markdown-body').css({'margin-top': '40px'})
-            }
-        }
-        if (breakpoint() == 'md') {
-            if (sd > 65 && dcheight > 0) {
-                if (!$('.subHeadWrapper').hasClass('shw-fixed')) {
-                    $('.subHeadWrapper').addClass('shw-fixed')
-                    $('.docContainer').addClass('dc-fixed')
-                    $('#sideBarCnt').addClass('ftm-fixed')
-                    $('#docHead').addClass('ch-fixed')
-                }
-                if (dcheight > document.body.clientHeight) {
-                    $('#sideBarCnt.ftm-fixed').css({'height': 'calc(100vh - 120px)'})
-                    $('#sideBarCnt.ftm-fixed #fullTreeMenuListContainer').css({'height': 'calc(100vh - 180px)'})
-                    $('.rightSideMenu.rsm-fixed').css({'height': 'calc(100vh - 110px)'})
+
+                // history fixed
+                if (sd < 65 && dcHeight + 48 > clientHeight) {
+                    if (!$('.history').hasClass('history-fixed')) {
+                        $('.history').addClass('history-fixed')
+                        $('#footerWrapper').css({'margin-top': '48px'})
+                    }
                 } else {
-                    $('#sideBarCnt.ftm-fixed').css({'height': (dcheight - 40) + 'px'})
-                    $('#sideBarCnt.ftm-fixed #fullTreeMenuListContainer').css({'height': (dcheight - 90) + 'px'})
-                    $('.rightSideMenu.rsm-fixed').css({'height': (dcheight - 40) + 'px'})
+                    $('.history').removeClass('history-fixed')
+                    $('#footerWrapper').css({'margin-top': '0px'})
                 }
-                $('.markdown-body').css({'margin-top': (100 + $('.container .head').height()) + 'px'})
-            } else {
-                $('.subHeadWrapper').removeClass('shw-fixed')
-                $('.docContainer').removeClass('dc-fixed')
-                $('#sideBarCnt').removeClass('ftm-fixed')
-                $('#docHead').removeClass('ch-fixed')
-                $('.markdown-body').css({'margin-top': '40px'})
-                $('#fullTreeMenuListContainer').css({'height': 'calc(100vh - 245px)'})
             }
+        } else {
+            $('.subHeadWrapper').css({'top': 'unset'})
+            $('#docHead').css({'top': 'unset'})
+            $('.sideBar').css({'padding-top': '20px'})
         }
     }
 
