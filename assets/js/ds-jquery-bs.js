71a7a60635b75ff90b52be5c6538629a7aa77f8d
Jenny <jenny@dynamsoft.com>
2020-12-09 16:59:05 +0800

    Revert "update header style --- use new design"
    This reverts commit 12d4220d59872a9e20469ca0d0f74f729681875b.

--- assets/js/ds-jquery-bs.js
+++ assets/js/ds-jquery-bs.js
@@ -17,29 +17,6 @@ $(function(){
 })
 
 $(function(){
-	/*header search*/
-	function headerSearch(){
-		var searchVal = '';
-		if(breakpoint()=='xs'){
-			searchVal = $("#xsSearch input[type='text']").val();
-		}else{
-			searchVal = $("#smSearch input[type='text']").val();
-		}
-		$('#txtSearch').val(searchVal);
-	    $("#btnSearch").click();
-	}
-
-	$(".headerSearch .headerSearchSubmit").click(function(){
-		headerSearch();
-	});
-
-	$(".headerSearch input[type='text']").bind('keypress',function(event){  
-			if(event.keyCode == "13")      
-			{
-					headerSearch();
-			}
-	});
-
 	/*xsMenu*/
 	$("#xsMenu .xsMenuToggle").click(function() {
 		$("#xsNav").slideToggle();
@@ -50,53 +27,27 @@ $(function(){
 			$("#xsNav .xsNavToggle .fa").removeClass("fa-caret-down").addClass("fa-caret-up");
 		},200)
 	});
-
 	$("#xsNav .xsNavToggle").click(function(){
 		$("#xsNav .xsNavToggle .fa").toggleClass('fa-caret-up');
 		$("#xsNav .xsNavToggle .fa").toggleClass('fa-caret-down');
 		$("#xsNav .products").slideToggle();
 	});
-
-	$("#xsNav .menuName").click(function () {
-		$(this).parent().toggleClass('on');
-		$(this).parent().find('.menuDetail').slideToggle(300);
-	});
 	
     /*smMenu*/
 	$("#smMenu .smMenuToggle").click(function() {
-		$("#mdMenu li").removeClass('on');
-		$("#subMenu").slideToggle(300);
-		$("#mdMenu li.products").toggleClass('on');
-		//  $("#subMenu").slideToggle(300);
-		//  //From sm to md&lg
-		//  $("#mdMenu li.products").toggleClass('on');
-		//  $("#mdMenu li.products i").toggleClass('fa-caret-up');
-		//  $("#mdMenu li.products i").toggleClass('fa-caret-down');
+		 $("#subMenu").slideToggle(300);
+		 //From sm to md&lg
+		 $("#mdMenu li.products").toggleClass('on');
+		 $("#mdMenu li.products i").toggleClass('fa-caret-up');
+		 $("#mdMenu li.products i").toggleClass('fa-caret-down');
 	});
 		
 	/*mdMenu*/
 	$("#mdMenu .mdMenuToggle").click(function(){
-		if ($(this).parent().hasClass('on')) {
-			$("#mdMenu li").removeClass('on');
-			$('body').removeClass('overviewHidden');
-			$("#menuMask").hide();
-		} else {
-			$("#menuMask").show();
-			$("#mdMenu li").removeClass('on');
-			$(this).parent().addClass('on');
-		}
-		if ($(this).hasClass('companyMenu')) {
-			$("#subMenu").slideUp();
-			$('#companySubMenu').slideToggle(300);
-		}
-		if ($(this).hasClass('proMenu')) {
-			$('#companySubMenu').slideUp();
-			$("#subMenu").slideToggle(300);
-		}
-		// $("#subMenu").slideToggle(300);
-		// $("#mdMenu li.products").toggleClass('on');
-		// $("#mdMenu li.products i").toggleClass('fa-caret-up');
-		// $("#mdMenu li.products i").toggleClass('fa-caret-down');
+		$("#subMenu").slideToggle(300);
+		$("#mdMenu li.products").toggleClass('on');
+		$("#mdMenu li.products i").toggleClass('fa-caret-up');
+		$("#mdMenu li.products i").toggleClass('fa-caret-down');
 	});
 	
 	/*subNav*/
@@ -108,7 +59,6 @@ $(function(){
 		$("#subNav .ct-list > li ul.ct-more").slideUp();
 		$("#subNav .ct-list > li .subNav-toggle i.show-xs").addClass('fa-angle-down').removeClass('fa-angle-up');
 	});
-
 	$("#subNav .subNav-toggle").click(function(){		
 		$(this).parent('li').siblings('li').removeClass('on');
 		$(this).parent('li').siblings('li').find("ul.ct-more").hide();
@@ -125,18 +75,6 @@ $(function(){
 		}
 	});
 	
-	/*smSearch*/
-	$("#smSearch .smSearchToggle").click(function(){
-		$(this).hide();
-		$("#smSearch .smSearchSubmit").show();
-		$("#smSearch input[type='text']").show().focus();
-		$("#subNav .ct-list > li").removeClass('on');
-		$("#subNav .ct-list > li ul.ct-more").hide();
-	});	
-
-	$('.mobileProductMenu .productLogo').on('click', function () {
-		$('.mobileProductDetailMenu').slideToggle(300);
-	})
 	
 	$(document).click(function(){
 		/*header*/
@@ -148,12 +86,8 @@ $(function(){
 			$("#xsNav .xsNavToggle .fa").removeClass("fa-caret-down").addClass("fa-caret-up");
 		},200);
 		$("#subMenu").slideUp('fast');
-		$("#menuMask").hide();
-		$('.headSubMenu').slideUp(300);
-		$("#mdMenu li").removeClass('on');
-		// $("#subMenu").slideUp('fast');
-		// $("#mdMenu li.products").removeClass('on');
-		// $("#mdMenu li.products i").removeClass('fa-caret-up').addClass('fa-caret-down');
+		$("#mdMenu li.products").removeClass('on');
+		$("#mdMenu li.products i").removeClass('fa-caret-up').addClass('fa-caret-down');
 		/*subNav*/
 		if(breakpoint()=='xs'){
 			$("#subNav .ct-list").slideUp();
@@ -163,18 +97,13 @@ $(function(){
 		$("#subNav .ct-list > li").removeClass('on');
 		$("#subNav .ct-list > li ul.ct-more").slideUp();
 		$("#subNav .ct-list > li .subNav-toggle i.show-xs").addClass('fa-angle-down').removeClass('fa-angle-up');
-		/*smSearch*/
-		$("#smSearch .smSearchSubmit").hide();
-		$("#smSearch input[type='text']").hide();
-		$("#smSearch .smSearchToggle").show();	
 	});
-
-	$("#xsHeader .xsMenuToggle, #xsNav, #smHeader .smMenuToggle, #mdMenu .mdMenuToggle, #subMenu, #subNav .subNav-xsToggle, #subNav .ct-list,.header-signin, .showMenuMore, .mobileProductMenu .productLogo, mobileProductDetailMenu").bind('click',function(e){
+	$("#xsHeader .xsMenuToggle, #xsNav, #smHeader .smMenuToggle, #mdMenu .mdMenuToggle, #subMenu, #subNav .subNav-xsToggle, #subNav .ct-list").bind('click',function(e){
 		stopPropagation(e);
 	});	
 	
 	/*toTop && sunNav-scroll-fixed*/
-  $("#toTop").hide();
+    $("#toTop").hide();
 	$(window).scroll(function(){
 	   var windowScTop = $(window).scrollTop();
 	   var subNavOffsetTop = $("#cookieAlert").is(":visible")?120:60;
@@ -233,21 +162,6 @@ $(function() {
  		resizeWindow();
 	});		
 });
-
-function isLogin() {
-	var DynamsoftToken = $.getCookie('DynamsoftToken')
-	if (DynamsoftToken) {
-		$('.header-visitor').hide()
-		$('.header-signin').show()
-		$('#lblusername').html($.getCookie('DynamsoftUserName'))
-		if ($.getCookie('DynamsoftRole') == '1' || $.getCookie('DynamsoftRole') == '4') {
-			$('#cp-header #action-nav .userbox-more li:nth-child(2)').show()
-		}
-	} else {
-		$('.header-signin').hide()
-		$('.header-visitor').show()
-	}
-}
 
 /*popup div*/
 function popup(id, e) {
