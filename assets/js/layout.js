a6abaf03e822fff2b269d7dc4820649c24fe7e1b
Jenny <jenny@dynamsoft.com>
2021-04-29 15:16:15 +0800

    change livechat id

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -143,15 +143,15 @@ $(document).ready(function(){
         stopPropagation(e);
     })
 
-    var commBtnInterval = setInterval(function() {
-        if ($('#comm100-float-button-2').css('background') != undefined && $('#comm100-float-button-2').css('background')!='') {
-            if ($('#comm100-float-button-2').css('background').indexOf('imgId=5000315') >= 0) {
-                $('#comm100-float-button-2').addClass('online');
-            } 
-            if ($('#comm100-float-button-2').css('background').indexOf('imgId=5000316') >= 0) {
-                $('#comm100-float-button-2').addClass('offline');
-            }
-            clearInterval(commBtnInterval)
-        }
-    }, 1000);
+    // var commBtnInterval = setInterval(function() {
+    //     if ($('#comm100-float-button-2').css('background') != undefined && $('#comm100-float-button-2').css('background')!='') {
+    //         if ($('#comm100-float-button-2').css('background').indexOf('imgId=5000315') >= 0) {
+    //             $('#comm100-float-button-2').addClass('online');
+    //         } 
+    //         if ($('#comm100-float-button-2').css('background').indexOf('imgId=5000316') >= 0) {
+    //             $('#comm100-float-button-2').addClass('offline');
+    //         }
+    //         clearInterval(commBtnInterval)
+    //     }
+    // }, 1000);
 })
