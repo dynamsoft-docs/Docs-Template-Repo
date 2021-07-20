875140270f253d85de143f65fb20710146dadce5
BBong119 <59595485+BBong119@users.noreply.github.com>
2021-07-20 17:57:02 +0800

    fix tree redir.

--- assets/js/index-banner.js
+++ assets/js/index-banner.js
@@ -309,22 +309,22 @@ function HighlightCurrentListForFullTree(searchListId, firstTime, searchUrl = do
             var curListATag =  $(curLi).children("a");
             
             if (!findExactPage) {
-                var ver = getUrlVars(document.URL)["ver"];
-                if (ver != undefined) {
-                    addParam(curListATag[0], ver);
-                }
-                else if (curPageRealVer != undefined) {
-                    addParam(curListATag[0], curPageRealVer);
-                }
-                else {
-                    window.location.href = curListATag[0].href;
-                }
-                // if (ver != undefined &&
-                //     ((ver != "latest" && pageStartVer != undefined && pageStartVer != "" && pageStartVer > ver) 
-                //     || (curPageRealVer != undefined && curPageRealVer != "" && ((ver == "latest" && ver != curPageRealVer) || (ver != "latest" && ver > curPageRealVer)))
-                //     )) {
+                // var ver = getUrlVars(document.URL)["ver"];
+                // if (ver != undefined) {
                 //     addParam(curListATag[0], ver);
                 // }
+                // else if (curPageRealVer != undefined) {
+                //     addParam(curListATag[0], curPageRealVer);
+                // }
+                // else {
+                //     window.location.href = curListATag[0].href;
+                // }
+                if (ver != undefined &&
+                    ((ver != "latest" && pageStartVer != undefined && pageStartVer != "" && pageStartVer > ver) 
+                    || (curPageRealVer != undefined && curPageRealVer != "" && ((ver == "latest" && ver != curPageRealVer) || (ver != "latest" && ver > curPageRealVer)))
+                    )) {
+                    addParam(curListATag[0], ver);
+                }
             }
         
             curListATag[0].style.color = '#fe8e14';
