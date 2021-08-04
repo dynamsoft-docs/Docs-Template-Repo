ac7f7f871fc0f0de9c4fc534076c3e6deaba12dd
Jenny <jenny@dynamsoft.com>
2021-08-04 15:09:17 +0800

    sidebar style fixed

--- assets/js/index-banner.js
+++ assets/js/index-banner.js
@@ -67,12 +67,14 @@ function FullTreeMenuList(generateDocHead, needh3 = true, pageStartVer = undefin
                 GenerateContentByHead(needh3);
                 //GenerateContentByHead(false);
             }
-            $('#fullTreeMenuListContainer').scrollTop($('#fullTreeMenuListContainer .activeLink').offset().top)
             var hiddenLayout = $('.docContainer, .sideBar, .history');
             for (var i = 0; i < hiddenLayout.length; i++) {
                 hiddenLayout[i].style.visibility = "visible";
             }
             init();
+
+            $('#fullTreeMenuListContainer').scrollTop($('#fullTreeMenuListContainer .activeLink').offset().top)
+            
 
             navWrap = document.getElementById("fullTreeMenuListContainer");
             var liAry = navWrap.getElementsByTagName("li");
@@ -163,12 +165,14 @@ function FullTreeMenuList(generateDocHead, needh3 = true, pageStartVer = undefin
                             GenerateContentByHead(needh3);
                             //GenerateContentByHead(false);
                         }
-                        $('#fullTreeMenuListContainer').scrollTop($('#fullTreeMenuListContainer .activeLink').offset().top)
                         var hiddenLayout = $('.docContainer, .sideBar, .history');
                         for (var i = 0; i < hiddenLayout.length; i++) {
                             hiddenLayout[i].style.visibility = "visible";
                         }
                         init();
+
+                        $('#fullTreeMenuListContainer').scrollTop($('#fullTreeMenuListContainer .activeLink').offset().top)
+                        
     
                         navWrap = document.getElementById("fullTreeMenuListContainer");
                         var liAry = navWrap.getElementsByTagName("li");
