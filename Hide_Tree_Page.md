07a29786a882d92414976d6994ed4ac2d5fe7071
Jenny <jenny@dynamsoft.com>
2022-07-25 15:03:56 +0800

    Revert "Merge pull request #16 from dynamsoft-docs/preview"
    This reverts commit d8539fe70777efccf6ed24c68805386e16affb06, reversing
    changes made to c0f48eeb4a9801f31f777502c95d4f653d7b4059.

--- Hide_Tree_Page.md
+++ Hide_Tree_Page.md
@@ -4,43 +4,13 @@ layout: tree-layout
 
 {%- if site.useVersionTree -%}
     <div id="version_tree_list">
-        {%- if site.edition -%}
-            {%- if site.edition == 'algorithm' -%}
-                {%- assign validVerInfo = site.data.product_version.version_info_list_algorithm -%}
-            {%- elsif site.edition == 'js' -%}
-                {%- assign validVerInfo = site.data.product_version.version_info_list_js -%}
-            {%- elsif site.edition == 'mobile' -%}
-                {%- assign validVerInfo = site.data.product_version.version_info_list_mobile -%}
-            {%- else -%}
-                {%- assign validVerInfo = site.data.product_version.version_info_list_desktop -%}
-            {%- endif -%}
-        {%- else -%}
-            {%- assign validVerInfo = site.data.product_version.version_info_list -%}
-        {%- endif -%}
-        {%- if site.data.product_version.useGroupedVersion -%}
-            {%- for verInfo in validVerInfo -%}
-                {%- if verInfo.child -%}
-                    {%- for childVersion in verInfo.child -%}
-                        {%- assign curId = "version_tree_" | append: childVersion | replace: " ", "_" | downcase -%}
-                        <ul class="version-tree-container " id="{{ curId }}">
-                            {%- include liquid_searchVersionTreeFile.html ver=childVersion curPath="" targetRelativePath="sidelist-full-tree.html" -%}
-                        </ul>
-                    {%- endfor -%}
-                {%- else -%}
-                    {%- assign curId = "version_tree_" | append: verInfo.value | replace: " ", "_" | downcase -%}
-                    <ul class="version-tree-container " id="{{ curId }}">
-                    {%- include liquid_searchVersionTreeFile.html ver=verInfo.value curPath="" targetRelativePath="sidelist-full-tree.html" -%}
-                    </ul>
-                {%- endif -%}
-            {%- endfor -%}
-        {%- else -%}
-            {%- for verInfo in validVerInfo -%}
-                {%- assign curId = "version_tree_" | append: verInfo | replace: " ", "_" | downcase -%}
-                <ul class="version-tree-container " id="{{ curId }}">
-                {%- include liquid_searchVersionTreeFile.html ver=verInfo curPath="" targetRelativePath="sidelist-full-tree.html" -%}
-                </ul>
-            {%- endfor -%}
-        {%- endif -%}
+        {%- assign validVerInfo = site.data.product_version.version_info_list -%}
+        {%- for verInfo in validVerInfo -%}
+            {%- assign curId = "version_tree_" | append: verInfo | replace: " ", "_" | downcase -%}
+            <ul class="version-tree-container " id="{{ curId }}">
+            {%- include liquid_searchVersionTreeFile.html ver=verInfo curPath="" targetRelativePath="sidelist-full-tree.html" -%}
+            </ul>
+        {%- endfor -%}
         <span id="complete_loading_tree"></span>
     </div>
 {%- endif -%}
--- assets/js/historymanage.js
+++ assets/js/historymanage.js
@@ -177,7 +177,7 @@ function GetVersionDiff(inputVer, compareVer)
     return diff;
 }
 
-function addParam (aTag, verText, fromSourse=null, needh3=false)
+function addParam (aTag, verText, fromSourse)
 {
     var hrefVal = aTag.href;
     var changeHref = hrefVal
@@ -202,23 +202,10 @@ function addParam (aTag, verText, fromSourse=null, needh3=false)
             changeHref = hrefVal+verStr
         }
     }
-    // && (verText == "latest" || verText == undefined)
+	
 	if (fromSourse == "sidebar") {
-        var currentDocDomain = document.URL.split("/docs/")[0] + '/docs/';
-        if (aTag.href.indexOf(currentDocDomain) < 0) {
-            window.location.href = aTag.href;
-            return;
-        }
         // request link
-        if (!$(aTag).hasClass("activeLink") || aTag.href.indexOf("/faq/") > 0) {
-            RequestNewPage(aTag, changeHref, needh3)
-        }
-    } else if (fromSourse == "docContainer") {
-        if (aTag.target == '_blank') {
-            window.open(changeHref);
-        } else {
-            findCurLinkOnFullTree(aTag, changeHref, needh3)
-        }
+        RequestNewPage(aTag, changeHref)
     } else {
         if (aTag.target == '_blank') {
             window.open(changeHref);
@@ -230,251 +217,46 @@ function addParam (aTag, verText, fromSourse=null, needh3=false)
 	return;
 }
 
-function RequestNewPage(aTag, paramLink, needh3=false, redirectUrl = null, onlyLoadContent=false) {
-    $("#articleContent").addClass("hidden")
+function RequestNewPage(aTag, paramLink) {
+    $("#articleContent").hide()
     $("#loadingContent").show()
-    var fetchUrl = redirectUrl ? redirectUrl : aTag.href
-    var oldLang = getCurrentUrlLang(document.URL)
-    fetch(fetchUrl).then(function(response) {
+    fetch(aTag.href).then(function(response) {
         return response.text()
     }).then(function(data) {
-        var inputVer = getUrlVars(paramLink)["ver"]
-        var otherVersions = $(data).find(".otherVersions > li")
+        document.title = $(data)[1].innerText
+        history.replaceState(null, null, paramLink)
+        if($(aTag).parents("li.collapseListStyle").length > 0) {
+            $(aTag).parents("li.collapseListStyle").addClass("expandListStyle").removeClass("collapseListStyle")
+        }
+        $("#fullTreeMenuListContainer .activeLink").removeClass("activeLink")
+        $(aTag).addClass("activeLink")
+        $("#articleContent").html($(data).find("#articleContent").html()).show()
+        $("#loadingContent").hide()
 
-        if (inputVer == "latest" || inputVer == undefined || otherVersions.length == 0 || redirectUrl) {
-            document.title = $(data)[1].innerText
-            !onlyLoadContent&&history.pushState(null, null, paramLink)
-
-            // remove old active link and li style
-            for(var i=0; i < $("#fullTreeMenuListContainer .activeLink").parents("li").length;i++) {
-                var obj = $("#fullTreeMenuListContainer .activeLink").parents("li")[i]
-                if ($(obj).hasClass("hasActiveLinkList")) {
-                    $(obj).removeClass("hasActiveLinkList")
-                }
-            }
-            $("#fullTreeMenuListContainer .activeLink").removeClass("activeLink")
-            // add current active link and li style
-            $(aTag).addClass("activeLink")
-            if($(aTag).parents("li.collapseListStyle").length > 0) {
-                $(aTag).parents("li.collapseListStyle").addClass("expandListStyle").removeClass("collapseListStyle")
-                $(aTag).parents("li.expandListStyle").find(" > ul").slideDown()
-            }
-            $(aTag).parents("li.expandListStyle").addClass("hasActiveLinkList")
-
-            var newLang = getCurrentUrlLang(document.URL)
-            if (oldLang != newLang) {
-                FilterLangFullTree()
-            }
-            
-            // show article content
-            var showRightSideMenu = $("#articleContent").hasClass("showRightSideMenu")
-            $("#articleContent").html($(data).find("#articleContent").html()).removeClass("hidden")
-            if (!showRightSideMenu) {
-                $("#articleContent").find(".rightSideMenu, .markdown-body").removeClass("showRightSideMenu")
-            }
-            $("#loadingContent").hide()
-            needh3 =  $(data).find("#articleContent").data("needh3") == true ? true : false
-            // if full tree has scroll bar, scroll to activelink position
-            var scrollDiv = document.getElementsByClassName("mainPage")[0]
-            if (scrollDiv.scrollHeight > scrollDiv.clientHeight) {
-                var activeLinkOffsetTop = $(".activeLink").offset().top - $(".mainPage").offset().top
-                if (activeLinkOffsetTop - scrollDiv.scrollTop + 40 > scrollDiv.clientHeight) {
-                    scrollDiv.scrollTop = activeLinkOffsetTop - 200
-                }
-            }
-            // add addParam click function for all a tags in article content
-            var articleContentATags = $("#articleContent").find("a")
-            var verArray = SearchVersion();
-            for (var i = 0; i < articleContentATags.length; i++)
-            {
-                articleContentATags[i].onclick = function(){
-                    addParam(this, verArray[0], 'docContainer', needh3); 
-                    return false;
-                };
-            }
-            // load breadcrumbs add right side menu
-            if ($("#AutoGenerateSidebar").length > 0) {
-                $('#fullTreeMenuListContainer').removeClass('needh3');
-                if (needh3) {
-                    $('#fullTreeMenuListContainer').addClass('needh3');
-                }
-                GenerateContentByHead(needh3);
-            }
+        if ($("#AutoGenerateSidebar").length > 0) {
+            GenerateContentByHead(false);
             $('#crumbs > ul').html($('#crumbs > ul > li').eq(0))
             initCrumbs()
-            // scroll to the start of article
-            var hash = paramLink.split("#").length > 1 ? paramLink.split("#")[1] : null
-            var sd = $(window).scrollTop()
-            if (hash) {
-                window.scrollTo(0, $("#" + hash).offset().top)
-            } else {
-                if (sd > 0) {
-                    window.scrollTo(0, sd > $('#overall-header').height() ? $('#overall-header').height() : sd)
-                }
-            }
-            
-            // load sample-code style
-            if($(".markdown-body .sample-code-prefix").length > 0 && getUrlVars(document.URL)["lang"]) {
-                var langs = getUrlVars(document.URL)["lang"].toLowerCase().trim().split(",")
-                if (langs) {
-                    if (langs.length == 1) {
-                        sampleCodeSingleLangInit(langs[0])
-                    } else {
-                        sampleCodeLangsInit(langs)
-                    }
-                }
-            } else if ($(".markdown-body .sample-code-prefix").length > 0) {
-                $('.markdown-body .sample-code-prefix + blockquote > ul > li:first-child').addClass('on')
-                $('.markdown-body .sample-code-prefix + blockquote > ol > li:first-child').addClass('on')
-                var template2Objs = $('.markdown-body .sample-code-prefix.template2 + blockquote')
-                for (var i=0; i<template2Objs.length; i++) {
-                    $(template2Objs[i]).find(">div").eq(0).addClass('on')
-                }
-            }
-            // load MathJax style
-            if ($("#articleContent").find("script").length>0) {
-                window.MathJax = null
-                window.MathJax = {
-                    tex: {
-                        inlineMath: [['$', '$'], ['\\(', '\\)']],
-                    },
-                    chtml:{
-                        scale: 1
-                    }
-                };
-                
-                (function () {
-                    var script = document.createElement('script');
-                    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
-                    script.async = true;
-                    document.head.appendChild(script);
-                })();
-            }
+        }
 
-            // file tree
-            if ($(".filetree h3").length > 0) {
-                if ($(window).outerWidth() > 1680) {
-                    if (breakpoint() == 'lg') {
-                        $('.markdown-body h2').css({'padding-top': $('#docHead').outerHeight() + 110 + 'px'})
-                        $('.markdown-body h2').css({'margin-top': -$('#docHead').outerHeight() - 80 + 'px'})
-                        $('.markdown-body h3').css({'padding-top': $('#docHead').outerHeight() + 110 + 'px'})
-                        $('.markdown-body h3').css({'margin-top': -$('#docHead').outerHeight() - 110 + 'px'})
-                        $('.markdown-body h4').css({'padding-top': $('#docHead').outerHeight() + 110 + 'px'})
-                        $('.markdown-body h4').css({'margin-top': -$('#docHead').outerHeight() - 110 + 'px'})
-                        $('.markdown-body h5').css({'padding-top': $('#docHead').outerHeight() + 110 + 'px'})
-                        $('.markdown-body h5').css({'margin-top': -$('#docHead').outerHeight() - 110 + 'px'})
-                    }
+        if($(".markdown-body .sample-code-prefix").length > 0 && getUrlVars(document.URL)["lang"]) {
+            var langs = getUrlVars(document.URL)["lang"].toLowerCase().trim().split(",")
+            if (langs) {
+                if (langs.length == 1) {
+                    sampleCodeSingleLangInit(langs[0])
                 } else {
-                    if (breakpoint() == 'lg') {
-                        $('.markdown-body h2').css({'padding-top': $('#docHead').outerHeight() + 90 + 'px'})
-                        $('.markdown-body h2').css({'margin-top': -$('#docHead').outerHeight() - 60 + 'px'})
-                        $('.markdown-body h3').css({'padding-top': $('#docHead').outerHeight() + 90 + 'px'})
-                        $('.markdown-body h3').css({'margin-top': -$('#docHead').outerHeight() - 90 + 'px'})
-                        $('.markdown-body h4').css({'padding-top': $('#docHead').outerHeight() + 90 + 'px'})
-                        $('.markdown-body h4').css({'margin-top': -$('#docHead').outerHeight() - 90 + 'px'})
-                        $('.markdown-body h5').css({'padding-top': $('#docHead').outerHeight() + 90 + 'px'})
-                        $('.markdown-body h5').css({'margin-top': -$('#docHead').outerHeight() - 90 + 'px'})
-                    } else {
-                        $('.markdown-body h2').css({'padding-top': '90px'})
-                        $('.markdown-body h2').css({'margin-top': '-60px'})
-                        $('.markdown-body h3').css({'padding-top': '90px'})
-                        $('.markdown-body h3').css({'margin-top': '-60px'})
-                        $('.markdown-body h4').css({'padding-top': '90px'})
-                        $('.markdown-body h4').css({'margin-top': '-90px'})
-                        $('.markdown-body h5').css({'padding-top': '90px'})
-                        $('.markdown-body h5').css({'margin-top': '-90px'})
-                    }
+                    sampleCodeLangsInit(langs)
                 }
             }
-
-            anchors.add();
-        } else {
-            var bestVerIndex = -1;
-            var verDiff = -1;
-            var bestVersion = inputVer;
-            
-            var historyList = $(data).find(".otherVersions");
-            var redirectTag = aTag
-            var matchSuccess = false
-            if (historyList != null) {
-                var listAry = historyList[0].getElementsByTagName("li");
-                for (var i = 0; i < listAry.length; i++) {
-                    var tmpVerText = listAry[i].innerText;
-                    var tmpVer = null;
-                    if (tmpVerText == "latest version"){
-                        tmpVer = "latest"
-                    }
-                    else{
-                        tmpVer = tmpVerText.replace('version ','');
-                    }
-                    if (tmpVer == inputVer) {
-                        var curTag = $(listAry[i]).children("a");
-                        redirectTag = curTag[0]
-                        if (paramLink.indexOf("?") > 0) {
-                            paramLink = redirectTag.href + "?" + paramLink.split("?")[1]
-                        } else if (paramLink.indexOf("#") > 0) {
-                            paramLink = redirectTag.href + "#" + paramLink.split("#")[1]
-                        }
-                        matchSuccess = true
-                        RequestNewPage(aTag, paramLink, needh3, redirectTag.href, onlyLoadContent)
-                        return
-                    } else {
-                        var tmpDiff = GetVersionDiff(inputVer, tmpVer);
-                        if (tmpDiff >= 0 && (tmpDiff < verDiff || verDiff < 0)){
-                            bestVerIndex = i;
-                            verDiff = tmpDiff;
-                            bestVersion = tmpVer;
-                        }
-                    }
-                }
-                
-                if (bestVerIndex >= 0 && !matchSuccess){
-                    var curTag = $(listAry[bestVerIndex]).children("a");
-                    redirectTag = curTag[0]
-                    if (paramLink.indexOf("?") > 0) {
-                        paramLink = redirectTag.href + "?" + paramLink.split("?")[1]
-                    } else if (paramLink.indexOf("#") > 0) {
-                        paramLink = redirectTag.href + "#" + paramLink.split("#")[1]
-                    }
-                    RequestNewPage(aTag, paramLink, needh3, redirectTag.href, onlyLoadContent)
-                    return
-                } else if (!matchSuccess) {
-                    RequestNewPage(aTag, paramLink, needh3, aTag.href, onlyLoadContent)
-                    return
-                }
+        } else if ($(".markdown-body .sample-code-prefix").length > 0) {
+            $('.markdown-body .sample-code-prefix + blockquote > ul > li:first-child').addClass('on')
+            $('.markdown-body .sample-code-prefix + blockquote > ol > li:first-child').addClass('on')
+            var template2Objs = $('.markdown-body .sample-code-prefix.template2 + blockquote')
+            for (var i=0; i<template2Objs.length; i++) {
+                $(template2Objs[i]).find(">div").eq(0).addClass('on')
             }
         }
     })
-}
-
-function findCurLinkOnFullTree(aTag, paramLink, needh3=false, onlyLoadContent=false) {
-    var fullTreeATags = $("#fullTreeMenuListContainer").find("a")
-    var targetHref = aTag.href.toLowerCase()
-    var curDocUrl = document.URL.toLowerCase()
-    targetHref = targetHref.indexOf("?") > 0 ? targetHref.split("?")[0] : (targetHref.indexOf("#") > 0 ? targetHref.split("#")[0] : targetHref) 
-    curDocUrl = curDocUrl.indexOf("?") > 0 ? curDocUrl.split("?")[0] : (curDocUrl.indexOf("#") > 0 ? curDocUrl.split("#")[0] : curDocUrl)
-    
-    if (curDocUrl == targetHref && (aTag.href.split("#").length > 1 || document.URL.split("#").length > 1)) {
-        var hash = aTag.href.split("#").length > 1 ? aTag.href.split("#")[1] : null
-        window.scrollTo(0, hash ? $("#" + hash).offset().top : 0)
-        !onlyLoadContent&&history.pushState(null, null, paramLink)
-    } else {
-        var flag = false
-        for(var i=0; i<fullTreeATags.length; i++) {
-            var searchHref = fullTreeATags[i].href
-            searchHref = searchHref.indexOf("index.html") > 0 ? searchHref.replace("index.html", "") : searchHref
-            targetHref = targetHref.indexOf("index.html") > 0 ? targetHref.replace("index.html", "") : targetHref
-            searchHref = searchHref.indexOf("?") > 0 ? searchHref.split("?")[0] : (searchHref.indexOf("#") > 0 ? searchHref.split("#")[0] : searchHref) 
-            if (fullTreeATags[i].offsetParent !== null && searchHref && searchHref.toLowerCase() == targetHref) {
-                flag = true
-                RequestNewPage(fullTreeATags[i], paramLink, needh3, null, onlyLoadContent)
-            }
-        }
-    
-        if (!flag) {
-            window.location.href = paramLink;
-        }
-    }
 }
 
 function changeVersion (liTag)
@@ -515,6 +297,7 @@ function changeVersion (liTag)
 function findNearestVersion(ver) {
     var versionList = $(".fullVersionInfo li")
     var bestVer = ver, verDiff=null
+    // console.log("versionList: " + versionList)
     for (var i=0; i<versionList.length; i++) {
         var tempVer = $(versionList[i]).text().toLowerCase()
         if (tempVer == "latest version"){
@@ -522,6 +305,7 @@ function findNearestVersion(ver) {
         } else{
             tempVer = tempVer.replace('version ','');
         }
+        // console.log("tempVer: " + tempVer)
         if (tempVer == ver) {
             return tempVer
         } else {
@@ -530,6 +314,9 @@ function findNearestVersion(ver) {
                 verDiff = tmpDiff;
                 bestVer = tempVer;
             }
+            // console.log('tmpDiff: ' + tmpDiff)
+            // console.log('verDiff: ' + verDiff)
+            // console.log('bestVersion: ' + bestVer)
         }
     }
     if (verDiff) {return bestVer} else {return "latest"}
