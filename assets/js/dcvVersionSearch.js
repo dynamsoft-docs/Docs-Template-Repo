4d5493675ec73a53f7e99f4e9fedfabe6d7e6c11
Jenny <jenny@dynamsoft.com>
2023-04-24 11:14:24 +0800

    Update dcvVersionSearch.js

--- assets/js/dcvVersionSearch.js
+++ assets/js/dcvVersionSearch.js
@@ -1,12 +1,12 @@
 var dcvVersionList = [
-  {
-    prodcut: "dbr",
-    version: "10.0",
-    dcvVersion: "2.0"
-  },
-  {
-    prodcut: "ddn",
-    version: "3.0",
-    dcvVersion: "2.0"
-  }
+  // {
+  //   prodcut: "dbr",
+  //   version: "10.0",
+  //   dcvVersion: "2.0"
+  // },
+  // {
+  //   prodcut: "ddn",
+  //   version: "3.0",
+  //   dcvVersion: "2.0"
+  // }
 ]
