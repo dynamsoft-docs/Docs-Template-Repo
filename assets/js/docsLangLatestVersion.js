80474fef86650694f254a985e81547c99fe88788
Dynamsoft-Henry <henry@dynamsoft.com>
2023-11-15 13:44:30 +0800

    Create docsLangLatestVersion.js

---
+++ assets/js/docsLangLatestVersion.js
@@ -0,0 +1,47 @@
+var docsLangLatestVersion = {
+    dbr: {
+        core: "10.0.20",
+        cpp: "10.0.20",
+        c: "9.6.30",
+        java: "9.6.30",
+        python: "9.6.30",
+        dotnet: "9.6.30",
+        android: "10.0.20",
+        ios: "10.0.20",
+        javaScript: "9.6.31",
+    },
+    dcv:{
+        core: "2.0.20",
+        cpp: "2.0.20",
+        android: "2.0.20",
+        ios: "2.0.20",
+        javaScript: "2.0.10",
+        xamarin: "1.0.5",
+        flutter: "1.2.2",
+        cordova: "1.0.4",
+        reactNative: "1.1.12"
+    },
+    dlr: {
+        core: "3.0.20",
+        cpp: "3.0.20",
+        java: "2.0.0",
+        dotnet: "2.0.0",
+        android: "2.2.20",
+        ios: "2.2.20",
+        javaScript: "2.2.31",
+    },
+    ddn: {
+        core: "2.0.20",
+        cpp: "2.0.20",
+        xamarin: "1.0.5",
+        android: "2.0.10",
+        ios: "2.0.10",
+        javaScript: "2.0.11",
+    },
+    dce: {
+        core: "4.0.1",
+        android: "4.0.1",
+        ios: "4.0.1",
+        javaScript: "4.0.0",
+    }
+}
