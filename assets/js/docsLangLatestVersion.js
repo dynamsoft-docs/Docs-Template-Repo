d039c282fddfa6a1ca1c75fdc705ffad2c6061ef
DMGithubPublisher <github@dynamsoft.com>
2025-05-30 17:30:34 +0800

    update to internal commit d7067de8

--- assets/js/docsLangLatestVersion.js
+++ assets/js/docsLangLatestVersion.js
@@ -6,36 +6,37 @@ var docsLangLatestVersion = {
         java: "9.6.40",
         python: "11.0.3000",
         dotnet: "11.0.3000",
-        android: "11.0.3000",
+        android: "11.0.3100",
         ios: "11.0.3000",
         javascript: "10.5.3000",
-        maui: "10.4.3000"
+        maui: "11.0.3100"
     },
     dcv:{
         core: "3.0.3000",
         cplusplus: "3.0.3000",
-        android: "3.0.3000",
+        android: "3.0.3100",
         dotnet: "3.0.3000",
         ios: "3.0.3000",
-        javascript: "2.6.1000",
+        javascript: "3.0.3000",
         xamarin: "1.0.5",
         flutter: "1.3.0",
         cordova: "1.0.5",
         reactNative: "1.1.14",
-        maui: "2.6.1000",
+        maui: "3.0.3100",
         nodejs: "2.6.1000",
         python: "3.0.3000"
     },
     mrz: {
-        android: "3.0.0",
+        android: "3.0.3100",
         ios: "3.0.0",
+        maui: "3.0.3100",
         javascript: "2.0"
     },
     dce: {
         core: "4.2.10",
         android: "4.3.0",
         ios: "4.3.0",
-        javascript: "4.1.1",
+        javascript: "4.1.2",
         maui: "4.2.200"
     }
 }
