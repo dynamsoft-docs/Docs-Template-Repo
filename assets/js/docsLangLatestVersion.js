01a4592a7af05ea176018eca21a84679e1935579
Mark <mark@dynamsoft.com>
2026-04-17 13:11:01 +0800

    for version 3.4.2001

--- assets/js/docsLangLatestVersion.js
+++ assets/js/docsLangLatestVersion.js
@@ -1,11 +1,11 @@
 var docsLangLatestVersion = {
     dbr: {
-        core: "11.4.2000",
-        cplusplus: "11.4.2000",
+        core: "11.4.2001",
+        cplusplus: "11.4.2001",
         c: "9.6.60",
-        java: "11.4.2000",
-        python: "11.4.2000",
-        dotnet: "11.4.2000",
+        java: "11.4.2001",
+        python: "11.4.2001",
+        dotnet: "11.4.2001",
         android: "11.4.1200",
         ios: "11.4.1200",
         javascript: "11.2.4000",
@@ -16,10 +16,10 @@ var docsLangLatestVersion = {
         cordova: "11.0.5200"
     },
     dcv:{
-        core: "3.4.2000",
-        cplusplus: "3.4.2000",
+        core: "3.4.2001",
+        cplusplus: "3.4.2001",
         android: "3.4.1200",
-        dotnet: "3.4.2000",
+        dotnet: "3.4.2001",
         ios: "3.4.1200",
         javascript: "3.2.5000",
         xamarin: "1.0.5",
@@ -28,8 +28,8 @@ var docsLangLatestVersion = {
         reactNative: "3.4.1100",
         maui: "3.4.1200",
         nodejs: "2.6.1000",
-        python: "3.4.2000",
-        java: "3.4.2000"
+        python: "3.4.2001",
+        java: "3.4.2001"
     },
     mrz: {
         android: "3.4.1000",
