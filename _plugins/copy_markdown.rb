b22624c93c716ced56090ea285fe688b749d3f74
Jenny-Jiani <70129215+Jenny-Jiani@users.noreply.github.com>
2026-08-27 11:16:50 +0800

    Update copy_markdown.rb

--- _plugins/copy_markdown.rb
+++ _plugins/copy_markdown.rb
@@ -1,14 +1,17 @@
 Jekyll::Hooks.register :site, :post_write do |site|
-  site.collections.each do |label, collection|
-    next unless label == 'articles'
-    collection.docs.each do |doc|
-      source_path = File.join(site.source, doc.relative_path)
-      next unless File.exist?(source_path)
+  source_root = site.source
+  source_prefix = "#{source_root}#{File::SEPARATOR}"
 
-      dest_path = doc.destination(site.dest).sub(/\.html$/, '.md')
-      dest_dir = File.dirname(dest_path)
-      FileUtils.mkdir_p(dest_dir)
-      FileUtils.cp(source_path, dest_path)
-    end
+  Dir.glob(File.join(source_root, '**', '*.md')).each do |source_path|
+    relative_path = source_path.delete_prefix(source_prefix)
+    path_parts = relative_path.split(File::SEPARATOR)
+    file_name = path_parts.pop
+
+    next unless path_parts.any? || file_name == 'index.md'
+    next if path_parts.any? { |part| part.start_with?('_') }
+
+    dest_path = File.join(site.dest, relative_path)
+    FileUtils.mkdir_p(File.dirname(dest_path))
+    FileUtils.cp(source_path, dest_path)
   end
 end
